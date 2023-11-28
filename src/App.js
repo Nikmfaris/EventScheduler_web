d4f9ac3c8181d483e44ebb52621bb6b2ae646ff6
Nikmfaris <nikfaris055@gmail.com>
2023-11-28 17:51:05 +0800

    add-minus

--- src/App.js
+++ src/App.js
@@ -2,21 +2,57 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const [number=0, setNumber] = useState(0);
+  
+  const handleSpeed = () => {
+    console.log(number)
+    const logoElement = document.getElementById('App-logo');
+    if (number >= 1) {
+      logoElement.style.animationDuration = `${"0."+number}s`;
+    }
+    if(number < 0){
+      logoElement.style.animationDuration = `${-(number)}s`;
+    }
+    if(number == 0){
+      logoElement.style.animationDuration = "0s";
+    }
+    
+  };
+  const increase = () => {
+    handleSpeed();
+    setNumber(number + 1);  
+  };
+
+  const decrease = () => {  
+    handleSpeed();
+    setNumber(number - 1);
+  };
+  const reset = () => {
+    
+    setNumber(0);
+    handleSpeed();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" id="App-logo" alt="logo" />
+
+        <div className='plus-minus'>
+          <div className='button'>
+            <button className='but plus' onClick={increase}>
+              +
+            </button>
+            <button className='but minus' onClick={decrease}>
+              -
+            </button>
+            <button className='but reset' onClick={reset}>
+              reset
+            </button>
+          </div>
+          <p>{number}</p>
+        </div>
       </header>
     </div>
   );
