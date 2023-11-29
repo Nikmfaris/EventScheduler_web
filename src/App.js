c66a4277d2dd8b0f4fcafa9dad031c1d9390827c
Nikmfaris <nikfaris055@gmail.com>
2023-11-29 20:20:06 +0800

    updating read me

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-
 import 'react-calendar/dist/Calendar.css';
 function App() {
   const [date, setDate] = useState(new Date())
