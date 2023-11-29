0a8b4cad6c9c3f091aba4890c53ef39428abd4e7
Nikmfaris <nikfaris055@gmail.com>
2023-11-29 21:47:11 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './App.css';
 function App() {
   const [date, setDate] = useState(new Date())
  
  
  return (
-  <div className="app">
-    <h1 className="header">Select a Date using React Calendar</h1>
+  <div className="container">
+    <h1 className="header">IIUM event Scheduler
+    </h1>
     <div className="calendar-container">
       <Calendar onChange={setDate} value={date}/>
     </div>
     <div className="text-center">
        Your selected date: {date.toDateString()}
     </div>
+    <h1>test</h1>
   </div>
    )
  
